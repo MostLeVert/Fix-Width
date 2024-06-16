c601abff62cced8110617d5bbd62478531f8aee3
Axel Jalonen <axeljalonen@gmail.com>
2024-06-15 22:05:45 -0700

    Initial commit

---
+++ popup.js
@@ -0,0 +1,26 @@
+async function triggerContentFunction() {
+    const tab = await getCurrentTab();
+    if (!tab) {
+      return;
+    }
+    try {
+      await chrome.scripting.insertCSS({
+        target: { tabId: tab.id },
+        css: `html{display:flex;justify-content:center}
+        body{max-width:1000px;margin:auto;}`
+      });
+    } catch (error) {
+      console.error("Error inserting CSS:", error);
+    }
+  }
+  
+  async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+  
+  document.getElementById("fix-width-btn").addEventListener("click", async () => {
+    await triggerContentFunction();
+  });
+  
