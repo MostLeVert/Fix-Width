bab45336fec33f6bac7ef83ff6743ad7f1bba84d
Axel Jalonen <axeljalonen@gmail.com>
2024-06-18 00:53:02 -0700

    windows 95 styling

--- popup.js
+++ popup.js
@@ -1,26 +1,25 @@
 async function triggerContentFunction() {
-    const tab = await getCurrentTab();
-    if (!tab) {
-      return;
-    }
-    try {
-      await chrome.scripting.insertCSS({
-        target: { tabId: tab.id },
-        css: `html{display:flex;justify-content:center}
-        body{max-width:1000px;margin:auto;}`
-      });
-    } catch (error) {
-      console.error("Error inserting CSS:", error);
-    }
+  const tab = await getCurrentTab()
+  if (!tab) {
+    return
   }
-  
-  async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  try {
+    await chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      css: `html{display:flex;justify-content:center}
+        body{max-width:1000px;margin:auto;}`,
+    })
+  } catch (error) {
+    console.error("Error inserting CSS:", error)
   }
-  
-  document.getElementById("fix-width-btn").addEventListener("click", async () => {
-    await triggerContentFunction();
-  });
-  
+}
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true }
+  let [tab] = await chrome.tabs.query(queryOptions)
+  return tab
+}
+
+document.getElementById("fix-width-btn").addEventListener("click", async () => {
+  await triggerContentFunction()
+})
