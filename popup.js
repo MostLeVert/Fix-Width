6a8a657d5c29dda6cc09fa4c5ebf5d6b0944d9d8
Axel Jalonen <axeljalonen@gmail.com>
2024-06-19 17:28:28 -0700

    Implement settings in localStorage, update theme, update icons

--- popup.js
+++ popup.js
@@ -1,13 +1,42 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const fixWidthBtn = document.getElementById("fix-width-btn")
+  fixWidthBtn.addEventListener("click", async () => {
+    await triggerContentFunction()
+  })
+})
+
 async function triggerContentFunction() {
   const tab = await getCurrentTab()
+
   if (!tab) {
+    console.error("TAB NOT FOUND")
     return
   }
+
+
   try {
+    let sizePreference = this.localStorage.getItem("width")
+    console.log(sizePreference)
+    let sizePreferenceMap = 100
+    switch (sizePreference) {
+      case "small":
+        sizePreferenceMap = 500
+        break
+      case "medium":
+        sizePreferenceMap = 1000
+        break
+      case "large":
+        sizePreferenceMap = 1500
+        break
+      default:
+        sizePreferenceMap = 50
+        break
+    }
+
     await chrome.scripting.insertCSS({
       target: { tabId: tab.id },
       css: `html{display:flex;justify-content:center}
-        body{max-width:1000px;margin:auto;}`,
+        body{max-width:${sizePreferenceMap}px;margin:auto;}`,
     })
   } catch (error) {
     console.error("Error inserting CSS:", error)
@@ -19,7 +48,3 @@ async function getCurrentTab() {
   let [tab] = await chrome.tabs.query(queryOptions)
   return tab
 }
-
-document.getElementById("fix-width-btn").addEventListener("click", async () => {
-  await triggerContentFunction()
-})
