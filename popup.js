8721cf3dd1ecbcb908fe63e72ea1bee76cf88285
Axel Jalonen <axeljalonen@gmail.com>
2024-06-20 17:16:56 -0700

    Rollback

--- popup.js
+++ popup.js
@@ -1,49 +1,26 @@
-window.addEventListener("DOMContentLoaded", function () {
-  const fixWidthBtn = document.getElementById("fix-width-btn")
-  fixWidthBtn.addEventListener("click", async () => {
-    await triggerContentFunction()
-  })
-})
-
 async function triggerContentFunction() {
-  const tab = await getCurrentTab()
-
-  if (!tab) {
-    console.error("TAB NOT FOUND")
-    return
-  }
-
-  try {
-    let sizePreference = this.localStorage.getItem("width")
-    console.log(sizePreference)
-    let sizePreferenceMap = 100
-    switch (sizePreference) {
-      case "small":
-        sizePreferenceMap = 500
-        break
-      case "medium":
-        sizePreferenceMap = 1000
-        break
-      case "large":
-        sizePreferenceMap = 1500
-        break
-      default:
-        sizePreferenceMap = 50
-        break
+    const tab = await getCurrentTab();
+    if (!tab) {
+      return;
     }
-
-    await chrome.scripting.insertCSS({
-      target: { tabId: tab.id },
-      css: `html{display:flex;justify-content:center}
-        body{max-width:${sizePreferenceMap}px;margin:auto;}`,
-    })
-  } catch (error) {
-    console.error("Error inserting CSS:", error)
+    try {
+      await chrome.scripting.insertCSS({
+        target: { tabId: tab.id },
+        css: `html{display:flex;justify-content:center}
+        body{max-width:1000px;margin:auto;}`
+      });
+    } catch (error) {
+      console.error("Error inserting CSS:", error);
+    }
   }
-}
-
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true }
-  let [tab] = await chrome.tabs.query(queryOptions)
-  return tab
-}
+  
+  async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+  
+  document.getElementById("fix-width-btn").addEventListener("click", async () => {
+    await triggerContentFunction();
+  });
+  
